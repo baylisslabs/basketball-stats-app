d63fa19839e879d4e0edcb43e861e81ed8c17d54
Chris Bayliss <chris@bayliss-it.com.au>
2024-06-13 15:24:29 +0800

    feat: refactor model to component

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,64 @@
+<script setup>
+import { reactive, onMounted, computed } from 'vue'
+import ConfirmClearDialog from './ConfirmClearDialog.vue';
+
+const storageKey = "counters";
+
+const state = reactive({
+  counters: [],
+  showConfirm: false
+})
+
+onMounted(() => {
+    const counters = localStorage.getItem(storageKey);
+    if (counters) {
+      state.counters = JSON.parse(counters);
+    } else {
+      clear();
+    }
+  }
+);
+
+const increment = (counter) => {
+  counter.value++;
+  save();
+};
+
+const decrement = (counter) => {
+  if (counter.value) {
+    counter.value--;
+  }
+  save();
+};
+
+const save = () => {
+  localStorage.setItem(storageKey, JSON.stringify(state.counters));
+};
+
+const clear = () => {
+  localStorage.removeItem(storageKey);
+  state.counters = [
+      //{ "label": "Screens", value: 0 },
+      { "label": "Rebounds", value: 0 },
+      { "label": "Outlet Pass", value: 0 },
+      { "label": "Steal / Intercepts", value: 0 },
+      { "label": "Block(no foul)", value: 0 },
+      { "label": "Shot Attempt\n(In the paint)", value: 0 },
+    ];
+};
+
+const total = computed(() => (counters) => {
+  var total = 0;
+  for (const c of counters) {
+    total += c.value;
+  }
+  return total
+});
+</script>
+
 <template>
   <div class="container mt-4">
-    <div v-for="c in counters" class="row d-flex align-items-center py-2 px-2">
+    <div v-for="c in state.counters" class="row d-flex align-items-center py-2 px-2">
       <div class="col-5">
         {{ c.label }}
       </div>
@@ -23,85 +81,19 @@
       <div class="col-2">
       </div>
       <div class="col d-flex justify-content-end">
-        {{ total(counters) }}
+        {{ total(state.counters) }}
       </div>
     </div>
     <div class="row d-flex align-items-center py-2 px-2">
       <div class="col d-flex justify-content-end">
-        <button class="btn btn-secondary mx-2" data-bs-target="#confirmClearModal" data-bs-toggle="modal">Clear</button>
+        <button class="btn btn-secondary mx-2" @click="state.showConfirm = true">Clear</button>
       </div>
     </div>
 
-    <div class="modal fade" id="confirmClearModal" aria-hidden="true" aria-labelledby="confirmClearModalLabel" tabindex="-1">
-      <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="confirmClearModalLabel">Confirm</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            This will reset all counters?
-          </div>
-          <div class="modal-footer">
-            <button class="btn btn-primary" @click="clear" data-bs-target="#confirmClearModal" data-bs-toggle="modal">Confirm</button>
-            <button class="btn btn-primary" data-bs-target="#confirmClearModal" data-bs-toggle="modal">Cancel</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
+    <ConfirmClearDialog
+      :show="state.showConfirm"
+      @shouldClose="state.showConfirm = false"
+      @confirmed="clear"/>
   </div>
 </template>
 
-<script>
-const storageKey = "counters";
-
-export default {
-  mounted() {
-    const counters = localStorage.getItem(storageKey);
-    if (counters) {
-      this.counters = JSON.parse(counters);
-    } else {
-      this.clear();
-    }
-  },
-  data() {
-    return {
-      counters: []
-    };
-  },
-  methods: {
-    increment(counter) {
-      counter.value++;
-      this.save();
-    },
-    decrement(counter) {
-      if (counter.value) {
-        counter.value--;
-      }
-      this.save();
-    },
-    total(counters) {
-      var total = 0;
-      for (const c of counters) {
-        total += c.value;
-      }
-      return total;
-    },
-    save() {
-      localStorage.setItem(storageKey, JSON.stringify(this.counters));
-    },
-    clear() {
-      localStorage.removeItem(storageKey);
-      this.counters = [
-        //{ "label": "Screens", value: 0 },
-        { "label": "Rebounds", value: 0 },
-        { "label": "Outlet Pass", value: 0 },
-        { "label": "Steal / Intercepts", value: 0 },
-        { "label": "Block(no foul)", value: 0 },
-        { "label": "Shot Attempt\n(In the paint)", value: 0 },
-      ];
-    }
-  },
-};
-</script>
---
+++ src/components/ConfirmClearDialog.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    show: Boolean
+});
+
+const emit = defineEmits(['confirmed', 'shouldClose'])
+
+const display = computed(() => {
+  return props.show ? "block" : "none";
+});
+
+const onCancel = () => {
+  emit("shouldClose")
+}
+
+const onConfirm = () => {
+  emit("confirmed")
+  emit("shouldClose")
+}
+</script>
+
+<template>
+  <div class="modal fade show" id="confirmClearModal" v-bind:style="{ display: display }" aria-hidden="true" aria-labelledby="confirmClearModalLabel" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="confirmClearModalLabel">Confirm</h1>
+          <button type="button" class="btn-close" @click="onCancel" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          This will reset all counters?
+        </div>
+        <div class="modal-footer">
+          <button class="btn btn-primary" @click="onConfirm">Confirm</button>
+          <button class="btn btn-primary" @click="onCancel">Cancel</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
